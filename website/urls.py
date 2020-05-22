381b61c9029285fbb7a77d0bde62fa718cd49b3d
HoopDead <polec0@wp.pl>
2020-05-22 20:48:45 +0200

    Added paggination

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = 'index'),
     path('business', views.business),
     path('entertainment', views.entertainment),
     path('health', views.health),
--- website/views.py
+++ website/views.py
@@ -12,22 +12,20 @@ newsapi = NewsApiClient(api_key = 'b0307aaaee13449f8a2e6b100a862b08')
 
 def index(request):
     form = api_request_query()
-    if request.method == "POST":
-        form = api_request_query(request.POST)
+    if request.method == "GET":
+        form = api_request_query(request.GET)
         if form.is_valid():
-            cd = form.cleaned_data
-            query = cd.get('query')
-            page = request.GET.get('page')
-            query_get = request.GET.get(query)
-            print(query_get)
-            print(page)
-            if page == None:
-                page = 1
-            print(query)
+            page = 1
+            page = int(request.GET.get('page'))
+            query = request.GET.get('query')
+            if page == 1:
+                pages = {'1': 1, '2': 2, '3': 3}
+            elif page > 1:
+                pages = {'1': page-1, '2': page, '3': page+1}
+            print(pages)
             all_articles = newsapi.get_everything(q = query, page = int(page), page_size = 25, language = 'en', sort_by = 'relevancy')
             num_of_pages = m.ceil(all_articles['totalResults']/25)
-            print(num_of_pages)
-            return render(request, 'website/index.html', {'form': form, 'all_articles': all_articles, 'query': query})
+            return render(request, 'website/index.html', {'form': form, 'all_articles': all_articles, 'pages': pages, 'query': query, 'page': page})
     return render(request, 'website/index.html', {'form': form})
 
 def science(request):
